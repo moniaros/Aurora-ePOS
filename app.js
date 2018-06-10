582e4d2a32671110e51f54d0cf299abea189307d
Tomasz Tracz <t@tracz.me>
2018-06-10 13:25:52 +0100

    Initial build

---
+++ app.js
@@ -0,0 +1,6 @@
+var express                 = require('express'),
+    bodyParser              = require('body-parser'),
+    mongoose                = require('mongoose'),
+    path                    = require('path')
+
+const LOCALCONF             = require('./local_conf.js')
